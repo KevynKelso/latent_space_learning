980b7d1d7b7d984cf6243093753a474c52887335
Kevyn Kelso <kevynkelso@outlook.com>
2023-11-14 08:21:42 -0700

    feat: ability to save as we go

--- generate_smile_classification_dataset.py
+++ generate_smile_classification_dataset.py
@@ -1,21 +1,27 @@
 import sys
+from os.path import isfile
 
 import cv2
 import numpy as np
 from PIL import Image
-from tqdm import tqdm
 
 from progressive_growing_of_gans.pretrained_gan import run_model
 from utils_data import load_real_samples, plot_faces
 
 
 def main(gan_pickle):
-    not_smiling = []
-    smiling_imgs = []
     smiling_latents = []
     not_smiling_latents = []
 
-    while len(smiling_imgs) < 1000:
+    while len(smiling_latents) < 100000:
+        if isfile("smiling.npz"):
+            smiling_latents = np.load("smiling.npz")["arr_0"]
+        if isfile("not_smiling.npz"):
+            not_smiling_latents = np.load("not_smiling.npz")["arr_0"]
+
+        print(len(smiling_latents))
+        print(smiling_latents.shape)
+
         num_images = 10
         # Generate latent vectors.
         latents = np.random.randn(num_images, 512)
@@ -27,23 +33,18 @@ def main(gan_pickle):
             gray_frame = np.array(Image.fromarray(np.uint8(img)).convert("L"))
             # increasing minNeighbors will decrease false positives only detecting 'wide' smiles
             smiles = smile_cascade.detectMultiScale(
-                gray_frame, scaleFactor=1.9, minNeighbors=40
+                gray_frame, scaleFactor=1.9, minNeighbors=50
             )
             if len(smiles) > 0:
-                smiling_imgs.append(np.uint8(img))
-                smiling_latents.append(latent)
+                latent = np.array([latent])
+                smiling_latents = np.append(smiling_latents, latent, axis=0)
             else:
-                not_smiling.append(np.uint8(img))
-                not_smiling_latents.append(latent)
+                latent = np.array([latent])
+                not_smiling_latents = np.append(not_smiling_latents, latent, axis=0)
 
-    np.savez_compressed("smiling.npz", smiling_latents)
-    np.savez_compressed("not_smiling.npz", not_smiling_latents)
-
-    print(len(smiling_imgs))
-    print(len(not_smiling))
-
-    plot_faces(smiling_imgs, 5)
-    plot_faces(not_smiling, 5)
+        if len(smiling_latents) and len(not_smiling_latents):
+            np.savez_compressed("smiling.npz", smiling_latents)
+            np.savez_compressed("not_smiling.npz", not_smiling_latents)
 
 
 def load_smiling(gan_pickle):
