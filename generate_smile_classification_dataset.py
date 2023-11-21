84d500b834e111bbd83d29798ccce1965cb49055
Kevyn Kelso <kevynkelso@outlook.com>
2023-11-21 09:15:28 -0700

    fix(dataset-gen): generate from empty array

--- generate_smile_classification_dataset.py
+++ generate_smile_classification_dataset.py
@@ -8,23 +8,26 @@ from PIL import Image
 from progressive_growing_of_gans.pretrained_gan import run_model
 from utils_data import load_real_samples, plot_faces
 
+num_images_to_generate = (
+    30  # Adjust this value based on how much vram you have. 30 Requires about 24GB.
+)
+smiling_data = "smiling.npz"
+not_smiling_data = "not_smiling.npz"
+
 
 def main(gan_pickle):
-    smiling_latents = []
-    not_smiling_latents = []
+    smiling_latents = np.array((0, 0))
+    not_smiling_latents = np.array((0, 0))
 
-    while len(smiling_latents) < 100000:
-        if isfile("smiling.npz"):
-            smiling_latents = np.load("smiling.npz")["arr_0"]
-        if isfile("not_smiling.npz"):
-            not_smiling_latents = np.load("not_smiling.npz")["arr_0"]
+    while len(smiling_latents) < 200000:
+        if isfile(smiling_data):
+            smiling_latents = np.load(smiling_data)["arr_0"]
+        if isfile(not_smiling_data):
+            not_smiling_latents = np.load(not_smiling_data)["arr_0"]
 
         print(len(smiling_latents))
-        print(smiling_latents.shape)
 
-        num_images = 10
-        # Generate latent vectors.
-        latents = np.random.randn(num_images, 512)
+        latents = np.random.randn(num_images_to_generate, 512)
         images = run_model(gan_pickle, latents)
 
         smile_cascade = cv2.CascadeClassifier("./gan_project/haarcascade_smile.xml")
@@ -43,12 +46,12 @@ def main(gan_pickle):
                 not_smiling_latents = np.append(not_smiling_latents, latent, axis=0)
 
         if len(smiling_latents) and len(not_smiling_latents):
-            np.savez_compressed("smiling.npz", smiling_latents)
-            np.savez_compressed("not_smiling.npz", not_smiling_latents)
+            np.savez_compressed(smiling_data, smiling_latents)
+            np.savez_compressed(not_smiling_data, not_smiling_latents)
 
 
 def load_smiling(gan_pickle):
-    smiling_latents = np.load("smiling.npz")["arr_0"]
+    smiling_latents = np.load(smiling_data)["arr_0"]
     print(smiling_latents)
 
     for latent in smiling_latents:
@@ -60,7 +63,7 @@ def load_smiling(gan_pickle):
 
 
 def average_smiling(gan_pickle):
-    smiling_latents = np.load("not_smiling.npz")["arr_0"]
+    smiling_latents = np.load(not_smiling_data)["arr_0"]
     print(smiling_latents.shape)
 
     avg_latent = smiling_latents.mean(axis=0)
@@ -70,4 +73,4 @@ def average_smiling(gan_pickle):
 
 
 if __name__ == "__main__":
-    average_smiling(sys.argv[1])
+    main(sys.argv[1])
